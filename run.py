4cf732fbfcf0365a1f0c65cf9af3621109fdcfb0
dorotheeasiimwe <dorotheeasiimwe@gmail.com>
2018-12-05 10:46:18 +0300

    update run file

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 
 from api.models.db import Connection
 from api.routes import Urls
 
 APP = Flask(__name__)
+CORS(APP)
 
 Urls.get_url(APP)
 APP.config['JWT_SECRET_KEY'] = 'secretKEY' 
